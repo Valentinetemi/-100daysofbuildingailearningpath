5f96aced0de4c91078ee43d512030d38840bfccb
Valentinetemi <valentinetemi32@gmail.com>
2025-11-19 09:43:34 +0100

    Playing with string spliting in python class

--- big0notation/constant.py
+++ big0notation/constant.py
@@ -1,3 +1,26 @@
-first_number = [2, 4, 6, 8, 10, 12]
+my_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
-print("The index of the fourth number in the array is ", first_number[3])
+print(my_list[1: 9])
+my_name = "At Summit University we shine like the sun"
+print(my_name[20: 30])
+
+my_name = "ewa"
+print(my_name[::-1])
+
+myPython = "python"
+name = myPython.split()
+print(name)
+
+myPython = list(myPython)
+print(myPython)
+myPython[0] = "J"
+print(myPython)
+
+
+
+name1 = myPython[0: 3] 
+print(name1)
+
+school = "Hmm, we go to school to learn"
+school_words = school.split()
+print(school_words)
