330b7bcfbc294a155efc95542c783cbae5ec4745
Valentinetemi <valentinetemi32@gmail.com>
2025-11-28 14:51:43 +0100

    webcam with opencv

---
+++ OpenCV/webcamecv.py
@@ -0,0 +1,19 @@
+import cv2
+
+#read webcam
+webcam = cv2.VideoCapture(2)
+
+
+
+
+#visualize webcam\
+
+while True:
+    ret, frame = webcam.read()
+    
+    cv2.imshow('frame', frame)
+    if  cv2.waitKey(40) & 0xFF == ord('q'):
+        break
+    
+webcam.realease()
+cv2.destroyAllWindows()
