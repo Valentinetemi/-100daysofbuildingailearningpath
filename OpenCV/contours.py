052c6a2d61a7d45ee44912bc00f4b602d9b62216
Valentinetemi <valentinetemi32@gmail.com>
2025-11-30 12:44:01 +0100

    BAHBAH BAH

--- OpenCV/contours.py
+++ OpenCV/contours.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import numpy as np
 
 
 img = cv.imread("image6.png")
@@ -6,8 +7,11 @@ img = cv.imread("image6.png")
 resized_img = cv.resize(img,(480,360))
 
 ret, thresh = cv.threshold(img, 127, 255, cv.THRESH_BINARY_INV)
-resized_img_c = cv.resize(thresh,(480,360))
 
+
+im2, contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+resized_img_c = cv.resize(thresh,(480,360))
 cv.imshow('img', resized_img)
 
 cv.imshow('thresh', resized_img_c)
