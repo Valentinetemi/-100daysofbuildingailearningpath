16a7cb9f3fff511e64a0478366fff1c02153afa9
Valentinetemi <valentinetemi32@gmail.com>
2025-12-03 16:37:24 +0100

    Change waitkey to 5

--- Air Button/main.py
+++ Air Button/main.py
@@ -10,14 +10,14 @@ mp_drawing = mp.solutions.drawing_utils
 cap = cv.VideoCapture(0)
 
 #Create a canvas to draw the hand landmarks
-canvas = np.zeros((480, 640, 3), dtype=np.uint8)
+canvas = np.zeros((480, 640, 3), dtype=np.uint8) # this is just the black image where we draw the line , later we overlay it on the webcame feed
 
 prev_x, prev_y = 0, 0 #stores previous finger positions
 
 while True:
     ret, frame = cap.read()
     if not ret:
-        break
+        break # if the frame is not read, break the loop
     
     frame = cv.flip(frame, 1) #flip the frame horizontally
     
@@ -28,7 +28,7 @@ while True:
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
             #Get index finger landmark (landmark 8)
-            x = int(hand_landmarks.landmark[8].x * frame.shape[1])
+            x = int(hand_landmarks.landmark[8].x * frame.shape[1]) #landmark 8 means = index fingertips, mediapipe gives coordinates in percentage, so we multiply to convert to pixels
             y = int(hand_landmarks.landmark[8].y * frame.shape[0])
             
             #Draw a small dot on the fingertips
@@ -36,7 +36,7 @@ while True:
             
             #draw line only if finger moved( not stationary)
             if prev_x != 0  and prev_y != 0:
-                cv.line(canvas, (prev_x, prev_y), (x, y), (0, 255, 0), 2)
+                cv.line(canvas, (prev_x, prev_y), (x, y), (0, 255, 0), 2) #draw a line from the previous point to the current points
                 
             #update previous position
             prev_x, prev_y = x, y
@@ -50,7 +50,7 @@ while True:
         
         cv.imshow('Hand Drawing', combined)
         
-        if cv.waitKey(1) & 0xFF == ord('q'):
+        if cv.waitKey(5) & 0xFF == ord('q'):
             break
         
         
