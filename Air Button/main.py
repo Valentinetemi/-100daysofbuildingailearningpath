f96c6c70db5741be2101339af38f49c532628735
Valentinetemi <valentinetemi32@gmail.com>
2025-12-03 16:39:37 +0100

    Added max_num cause it was hanging

--- Air Button/main.py
+++ Air Button/main.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.7)
+hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7, min_tracking_confidence=0.7)
 mp_drawing = mp.solutions.drawing_utils
 
 cap = cv.VideoCapture(0)
